14d51ad7e920a2c4263e6b2f39b6b42d18c3ab2d
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-02-11 19:46:35 +0700

    Bổ sung project CutieShop.API.DB - API giao tiếp với CSDL.

---
+++ CutieShop/CutieShop.API.DB/Models/DAO/DAO.cs
@@ -0,0 +1,46 @@
+﻿//ReSharper disable All
+using System;
+using System.Collections.Generic;
+
+namespace CutieShop.API.DB.Models.DAO
+{
+    public abstract class DAO<TId, TObj> : IDAO<TId, TObj>
+    {
+        private dynamic DbContext;
+
+        protected DAO(dynamic dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public bool Create(TObj obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TObj Read(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TObj> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(TObj obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+        }
+    }
+}
---
+++ CutieShop/CutieShop.API.DB/Models/DAO/IDAO.cs
@@ -0,0 +1,15 @@
+﻿//ReSharper disable All
+using System;
+using System.Collections.Generic;
+
+namespace CutieShop.API.DB.Models.DAO
+{
+    interface IDAO<TId, TObj> : IDisposable
+    {
+        bool Create(TObj obj);
+        TObj Read(TId id);
+        IEnumerable<TObj> ReadAll();
+        bool Update(TObj obj);
+        bool Delete(TId id);
+    }
+}
