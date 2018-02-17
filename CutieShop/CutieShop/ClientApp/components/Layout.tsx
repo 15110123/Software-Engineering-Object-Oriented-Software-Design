7d02d0f4af28c60a84aa13fcc21d74f6c338e2bc
leminhtanvatc <leminhtanvatc@gmail.com>
2018-02-17 15:39:45 +0700

    Thêm Header component (React) trong LayoutComponents

--- CutieShop/CutieShop/ClientApp/components/Layout.tsx
+++ CutieShop/CutieShop/ClientApp/components/Layout.tsx
@@ -1,6 +1,7 @@
 //ReSharper disable All
 import * as React from 'react';
 import { ChatBox } from "./chatBox/ChatBox";
+import {Header} from "./LayoutComponents/Header"
 
 export interface LayoutProps {
     children?: React.ReactNode;
@@ -9,18 +10,7 @@ export interface LayoutProps {
 export class Layout extends React.Component<LayoutProps, {}> {
     public render() {
         return <div>
-            <div className='container-fluid'>
-                <div className='row'>
-                    <div className='col-sm-3'>
-                        {
-                            //Add nav here
-                        }
-                    </div>
-                    <div className='col-sm-9'>
-                        {this.props.children}
-                    </div>
-                </div>
-            </div>
+            <Header/>
             <ChatBox />
         </div>
     }
---
+++ CutieShop/CutieShop/ClientApp/components/LayoutComponents/Header.tsx
@@ -0,0 +1,35 @@
+﻿import * as React from "react"
+import "../../css/Header/header.css"
+
+export class Header extends React.Component<{}, {}> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div id="header">
+                   <div id="logo">
+                       <img src="/img/svg/Logo.svg" id="imgLogo"/>
+                   </div>
+                   <div id="logoText">
+                       <label>Thế giới thú cưng</label>
+                   </div>
+                   <div id="searchBar">
+                       <div id="searchInput">
+                           <button id="submitBtn" form="SearchForm"></button>
+                           <form method="GET" id="SearchForm">
+                               <input name="keyword" id="searchText" placeholder="banh chạy, lồng mica, dây xích chó,..."/>
+                           </form>
+                       </div>
+                   </div>
+                   <div id="navBar">
+                       <div className="row">
+                           <div className="col-sm-1">
+                           </div>
+                           <div className="col-sm-1">
+                           </div>
+                       </div>
+                   </div>
+               </div>;
+    }
+}
